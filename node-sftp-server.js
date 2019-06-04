e4584551b81b8851f5a895cd68691b46972110cf
Tyler Stein <tyler.stein1@yahoo.ca>
2019-06-04 11:43:27 -0400

    fixed function comparison symbol

--- node-sftp-server.js
+++ node-sftp-server.js
@@ -135,7 +135,7 @@ var SFTPServer = (function(superClass) {
     };
 
     // Expose option for user-defined debug implementation
-    if (typeof options.debug === Function) {
+    if (typeof options.debug === 'function') {
       debug = options.debug;
     } else if (options.debug) {
       debug = function(msg) { console.log(msg); };
